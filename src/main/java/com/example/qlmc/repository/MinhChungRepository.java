0fa5591f65ec8e030fcea7aaef238684003efe3c
Nguyen Thanh Cong <ntcong.20it5@vku.udn.vn>
2024-08-05 18:23:38 +0700

    0508

--- src/main/java/com/example/qlmc/repository/MinhChungRepository.java
+++ src/main/java/com/example/qlmc/repository/MinhChungRepository.java
@@ -15,10 +15,6 @@ public interface MinhChungRepository extends JpaRepository<MinhChung, Long> {
     @Query(value = "SELECT mc.*, kmc.sohieu, kmc.ten_mc, kmc.linkluutru FROM minhchung mc JOIN khominhchung kmc ON kmc.id_kmc = mc.id_kmc WHERE mc.id_goiy = :idGoiY;", nativeQuery =  true)
     List<Object[]> findAllByIdGoiY(@Param("idGoiY") int idGoiY);
 
-
-    // @Query(value="DELETE FROM minhchung WHERE id_mc = :idMc; CREATE TEMPORARY TABLE temp_table AS SELECT child_ma_mc, LPAD(@row_num := @row_num + 1, 2, '0') AS new_stt FROM minhchung , (SELECT @row_num := 0) AS r WHERE minhchung.parent_ma_mc =: parentMaMc ORDER BY CAST(child_ma_mc AS UNSIGNED); UPDATE minhchung JOIN temp_table ON minhchung.child_ma_mc = temp_table.child_ma_mc SET minhchung.child_ma_mc = temp_table.new_stt WHERE parent_ma_mc = :parentMaMc; DROP TEMPORARY TABLE temp_table;SELECT mc.*, kmc.sohieu, kmc.ten_mc, kmc.linkluutru FROM minhchung mc JOIN khominhchung kmc ON kmc.id_kmc = mc.id_kmc WHERE mc.id_goiy = :idGoiY;", nativeQuery = true)
-    // List<Object[]> deleteAndGetData(@Param("idGoiY") int idGoiY, @Param("idMc") int idMc,@Param("parentMaMc") String parentMaMc);
-
     @Modifying
     @Transactional
     @Query(value = "DELETE FROM minhchung WHERE id_mc = :idMc", nativeQuery = true)
