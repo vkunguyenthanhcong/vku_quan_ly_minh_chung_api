84fff578cbfa5e480c9f859ddf7477f7cde869fc
vkunguyenthanhcong <ntcong.20it5@vku.udn.vn>
2024-09-10 13:15:05 +0700

    sua entity Nganh

--- src/main/java/com/example/qlmc/entity/Nganh.java
+++ src/main/java/com/example/qlmc/entity/Nganh.java
@@ -9,10 +9,36 @@ import jakarta.persistence.Table;
 @Table(name = "nganh")
 public class Nganh {
     @Id
-    @Column(name="ma_nganh")
+    @Column (name="id_nganh")
+    private int idNganh;
+
+    @Column (name="ma_nganh")
     private String maNganh;
-    @Column(name="ten_nganh")
+
+    @Column (name="ten_nganh")
     private String tenNganh;
+
+    @Column (name="ma_khoa")
+    private String maKhoa;
+
+    @Column (name="trinhdodaotao")
+    private String trinhDo;
+
+    public Nganh(int idNganh, String maKhoa, String maNganh, String tenNganh, String trinhDo) {
+        this.idNganh = idNganh;
+        this.maKhoa = maKhoa;
+        this.maNganh = maNganh;
+        this.tenNganh = tenNganh;
+        this.trinhDo = trinhDo;
+    }
+
+    public int getIdNganh() {
+        return idNganh;
+    }
+
+    public void setIdNganh(int idNganh) {
+        this.idNganh = idNganh;
+    }
 
     public String getMaNganh() {
         return maNganh;
@@ -28,5 +54,28 @@ public class Nganh {
 
     public void setTenNganh(String tenNganh) {
         this.tenNganh = tenNganh;
+    }
+
+    public String getMaKhoa() {
+        return maKhoa;
+    }
+
+    public void setMaKhoa(String maKhoa) {
+        this.maKhoa = maKhoa;
+    }
+
+    public String getTrinhDo() {
+        return trinhDo;
+    }
+
+    public void setTrinhDo(String trinhDo) {
+        this.trinhDo = trinhDo;
+    }
+
+    public Nganh(String maNganh, String tenNganh, String maKhoa, String trinhDo) {
+        this.maNganh = maNganh;
+        this.tenNganh = tenNganh;
+        this.maKhoa = maKhoa;
+        this.trinhDo = trinhDo;
     }
 }
